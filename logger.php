8a265da92595964288612015e0b66967fdb4e85d
Matteo Crippa <m.crippa@magneticmedia.com>
2015-09-09 21:41:12 +0200

    base php script

---
+++ logger.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  swiftRemoteLoggerPhp
+
+  Write down to disk each call in a separate log file with timestamp name
+*/
+
+header('Content-disposition: attachment; filename=response.json');
+header('Content-type: application/json');
+
+$password = 'password';
+
+$path = 'logs/';
+
+if($_POST['password'] == $password){
+
+  if($_POST['data']){
+
+    $data = $_POST['data'];
+    $udid = $_POST['udid'];
+    $app = $_POST['app'];
+
+    file_put_contents($path.$app.'/'.$udid.'.log', $data);
+
+    echo json_encode(array('ok'=>'logged'));
+
+  }else{
+    echo json_encode(array('error'=>'missing data'));
+  }
+
+
+}else{
+  echo json_encode(array('error'=>'wrong password'));
+}
